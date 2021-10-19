1637185fc0a74584e660d39cb814405b94cbf455
NilsForsse¨ <forssennils@gmail.com>
2021-10-19 10:27:09 +0200

    ladda upp

--- Hello_World.py
+++ Hello_World.py
@@ -1,1 +1,28 @@
-print("Hello World!")
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+cv2.namedWindow("test")
+
+img_counter = 0
+
+ret, frame = cam.read()
+if not ret:
+    print("failed to grab frame")
+
+(h, w, _) = frame.shape
+for i in range(h):
+    for j in range(w):
+        print(frame[i,j])
+
+
+cv2.imshow("test", frame)
+
+k = cv2.waitKey(0)
+if k%256 == 27:
+    # ESC pressed
+    print("Escape hit, closing...")
+
+cam.release()
+
+cv2.destroyAllWindows()
